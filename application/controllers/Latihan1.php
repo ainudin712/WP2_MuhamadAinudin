b2ad7e2149fdea64d616ff1b8277e7fd31c68ad9
ainudin712 <ainudin712@gmail.com>
2023-10-09 20:57:16 +0700

    Pertemuan 4

---
+++ application/controllers/Latihan1.php
@@ -0,0 +1,41 @@
+<?php
+class Latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "Selamat Datang...... Selamat Belajar Web Programming";
+        //$this->load->view('view-latihan');
+    }
+    
+    public function penjumlahan($n1, $n2)
+    {
+        // $this->load->model('Model_latihan1');
+    //     $hasil= $this->Model_latihan1->jumlah($n1, $n2);
+    //     echo "Hasil Penjumlahan dari ". $n1 ." + ". $n2 ." = ".$hasil;
+    // }
+
+    // public function perkalian($n1, $n2)
+    // {
+    //     $this->load->model('Model_latihan1');
+    //     $hasil= $this->Model_latihan1->kali($n1, $n2);
+    //     echo "Hasil Perkalian dari ". $n1 ." * ". $n2 ." = ".$hasil;
+    // }
+
+    // public function pengurangan($n1, $n2)
+    // {
+    //     $this->load->model('Model_latihan1');
+    //     $hasil= $this->Model_latihan1->kurang($n1, $n2);
+    //     echo "Hasil Perkalian dari ". $n1 ." * ". $n2 ." = ".$hasil;
+    // }
+
+    // public function penjumlahan($n1, $n2)
+    // {
+        $this->load->model('Model_latihan1');
+
+        $data['nilai1'] = $n1;
+        $data['nilai2'] = $n2;
+        $data['hasil'] = $this->Model_latihan1->jumlah($n1, $n2);
+
+        $this->load->view('view-latihan', $data);
+    }
+}
